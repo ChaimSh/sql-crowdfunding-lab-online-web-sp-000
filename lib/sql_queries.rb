ec8c5ac6209ca552315841d8ce88bdf94719dc5e
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-15 00:47:42 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,7 @@ GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT name, age SUM(pledges.amount) FROM user
+"SELECT name, age, SUM(pledges.amount) FROM user
 JOIN pledges ON user.id = pledges.user_id
 GROUP BY user.name;"
 end
